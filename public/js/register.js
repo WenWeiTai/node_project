2097c4687a8920677a9af53de80fb40d582a0a80
WenWeiTai <375354018@qq.com>
2018-11-27 10:32:44 +0800

    完成注册页面正则匹配判断和密码显示按钮功能

--- public/js/register.js
+++ public/js/register.js
@@ -3,10 +3,15 @@ window.onload = function() {
     user: $(".username"),
     pwd: $(".pwd"),
     nickname: $(".nickname"),
+    phone: $(".phone"),
+    surePwd: $(".surePwd"),
+    lookpwd: $(".lookpwd"),
+    looksurepwd: $(".looksurepwd"),
 
     reg_user: /^[\u4e00-\u9fa5]{2,4}$/,
     reg_pwd: /^[a-zA-Z]\w{5,11}$/,
-    reg_nickname: /^([\u4e00-\u9fa5]|\w){2,10}$/
+    reg_nickname: /^([\u4e00-\u9fa5]|\w){2,10}$/,
+    reg_phone: /^1[3-8]\d{9}$/
   };
   new RegRegister(obj);
 };
@@ -16,10 +21,15 @@ class RegRegister {
     this.user = obj.user;
     this.pwd = obj.pwd;
     this.nickname = obj.nickname;
+    this.phone = obj.phone;
+    this.surePwd = obj.surePwd;
+    this.lookpwd = obj.lookpwd;
+    this.looksurepwd = obj.looksurepwd;
 
     this.reg_user = obj.reg_user;
     this.reg_pwd = obj.reg_pwd;
     this.reg_nickname = obj.reg_nickname;
+    this.reg_phone = obj.reg_phone;
 
     this.flag = false;
     this.init();
@@ -29,9 +39,12 @@ class RegRegister {
     this.regUser();
     this.regPwd();
     this.regNickname();
+    this.regPhone();
+    this.surePassword(this.surePwd);
     this.checkSubmit();
 
-    this.lookPwd();
+    this.lookPwd(this.lookpwd,this.pwd);
+    this.lookPwd(this.looksurepwd,this.surePwd);
   }
 
   regUser() {
@@ -46,27 +59,43 @@ class RegRegister {
     this.checkIpu(this.nickname, this.reg_nickname);
   }
 
-  lookPwd(){
-    var _this = this;
-    $('.look').click(function(){
-      var type = _this.pwd.attr('type');
-      if(type == 'password'){
-        _this.pwd.attr('type','text');
-      }else{
-        _this.pwd.attr('type','password');
-      }
-    })
+  regPhone() {
+    this.checkIpu(this.phone, this.reg_phone);
   }
 
-
-  //表单submit验证
-  checkSubmit() {
+  //两次密码确认
+  surePassword(iptName) {
     var _this = this;
-    $("form").submit(function() {
-      if (_this.flag) {
-        return true;
+    iptName.blur(function() {
+      if (_this.pwd.val()) {
+        if (_this.pwd.val() == $(this).val()) {
+          $(this)
+            .next("p")
+            .html("密码一致")
+            .css({ color: "green" });
+          _this.flag = true;
+        } else {
+          $(this)
+            .next("p")
+            .html("两次密码不一致，请重新输入")
+            .css({ color: "red" });
+          _this.flag = false;
+        }
       } else {
-        return false;
+        _this.flag = false;
+      }
+    });
+  }
+
+  //查看密码
+  lookPwd(ele,iptName) {
+    var _this = this;
+    ele.click(function() {
+      var type =iptName.attr("type");
+      if (type == "password") {
+       iptName.attr("type", "text");
+      } else {
+       iptName.attr("type", "password");
       }
     });
   }
@@ -92,6 +121,26 @@ class RegRegister {
           .focus()
           .css({ borderColor: "red" });
         _this.flag = false;
+      }
+    });
+  }
+
+  //表单submit验证
+  checkSubmit() {
+    var _this = this;
+    $("form").submit(function() { 
+      if (
+        _this.flag &
+        (_this.user.val() != '') &
+        (_this.pwd.val() != '') &
+        (_this.nickname.val() != '') &
+        (_this.phone.val() != '') &
+        (_this.surePwd.val() != '') & 
+        (_this.surePwd.val() == _this.pwd.val())
+      ) {
+        return true;
+      } else {
+        return false;
       }
     });
   }
